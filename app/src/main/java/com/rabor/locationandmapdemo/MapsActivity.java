eb4a3ec6de4f4895cb6e858be2548815c1614df6
lrabor <lrabor@coleman.edu>
2015-10-06 16:36:51 -0700

    Initial commit

---
+++ app/src/main/java/com/rabor/locationandmapdemo/MapsActivity.java
@@ -0,0 +1,36 @@
+package com.rabor.locationandmapdemo;
+
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.widget.Toast;
+
+
+public class MapsActivity extends Activity {
+    static final LatLng TutorialsPoint = new LatLng(21 , 57);
+    private GoogleMap googleMap;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+        try {
+            if (googleMap == null) {
+                googleMap = ((MapFragment) getFragmentManager().
+                        findFragmentById(R.id.map)).getMap();
+            }
+            googleMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
+            Marker TP = googleMap.addMarker(new MarkerOptions().
+                    position(TutorialsPoint).title("TutorialsPoint"));
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+}
